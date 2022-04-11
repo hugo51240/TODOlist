2336ce49fdd1a21a23b3e7aed4e5db1d93ec9884
Patrice Adam <p.adam@infosoft.com>
2022-04-11 14:00:04 +0200

    fix

--- js/controller.js
+++ js/controller.js
@@ -8,7 +8,7 @@
     startSpinner();
     
     //fetch fresh data
-    var networkUpdate = fetch('https://clinquant-truffle-b2689f.netlify.app/').then(function(response){
+    var networkUpdate = fetch('http://127.0.0.1:5501/').then(function(response){
         return response.json();
     }).then(function(data)
     {
@@ -19,7 +19,7 @@
     //fetch cached data
     // reprendre le http generer par netilfy
     // 'http://127.0.0.1:5501/'
-    caches.match('https://clinquant-truffle-b2689f.netlify.app/').then(function(response)
+    caches.match('http://127.0.0.1:5501/').then(function(response)
     {
         if(!response)throw Error("no data");
         return response.json;
